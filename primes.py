12dc0f1c8f15c3a2b920d26f331b7ccfe10882cb
Vanessa Neboh <vanessa.neboh@kcl.ac.uk>
2022-12-08 20:52:38 +0000

    add primes function

--- primes.py
+++ primes.py
@@ -2,5 +2,16 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+    count = 0
+    if number_of_primes <= 0:
+        raise ValueError("cannot have zero or negative number of primes")
+    while len(list) != number_of_primes:
+        count += 1
+        for i in count:
+            if i != 2 and i != 3 and i != 5 and i != 7:
+                if i/1 != 1 and i%2 != 0 and i%3 != 0 and i%5 != 0 and i%7 != 0:
+                    list.append(i)
+
+
     list = []
     return list
